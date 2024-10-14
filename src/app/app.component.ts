20783a41b8570b8bdcfac4ca38a3c9f8a9cfb09b
Giancarlo Illescas <giancarlo.hoyos@gmail.com>
2024-10-14 11:41:29 -0500

    Rename Navbar component to Header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
-import { NavbarComponent } from "./layout/navbar/navbar.component";
+import { HeaderComponent } from "./layout/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent, NavbarComponent],
+  imports: [RouterOutlet, SidebarComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
