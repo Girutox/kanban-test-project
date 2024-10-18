6f768a32538fa2cc84e6828a3f103f40d7126835
Giancarlo Illescas <giancarlo.hoyos@gmail.com>
2024-10-18 11:22:35 -0500

    mend

---
+++ src/app/UI/SVG/icon-vertical-ellipsis/icon-vertical-ellipsis.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-vertical-ellipsis',
+  standalone: true,
+  imports: [],
+  templateUrl: './icon-vertical-ellipsis.component.svg',
+  styleUrl: './icon-vertical-ellipsis.component.scss'
+})
+export class IconVerticalEllipsisComponent {
+
+}
