e1090d242307f51d54fe8a68f9dae3ade1fb9168
Giancarlo Illescas <giancarlo.hoyos@gmail.com>
2024-10-18 11:21:28 -0500

    Add floating card first version

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,20 +1,27 @@
-import { ChangeDetectionStrategy, Component, computed, effect, inject, input, OnInit } from '@angular/core';
+import { Component, computed, inject, input, signal } from '@angular/core';
 import { CustomButtonComponent } from "../../UI/custom-button/custom-button.component";
 import { BoardService } from '../../board.service';
+import { IconVerticalEllipsisComponent } from "../../UI/SVG/icon-vertical-ellipsis/icon-vertical-ellipsis.component";
+import { FloatingCardComponent } from '../../UI/floating-card/floating-card.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CustomButtonComponent],
+  imports: [CustomButtonComponent, IconVerticalEllipsisComponent, FloatingCardComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   boardService = inject(BoardService);
   sidebarHidden = input<boolean>(false);
+  showFloatingCard = signal(false);
 
   activeBoardName = this.boardService.activeBoardName;
   isBoardEmpty = computed(() => {    
     return (this.activeBoardName()) ? this.boardService.getBoardColumns(this.activeBoardName()).length == 0 : true;
   });
+
+  onToggleFloatingCard() {
+    this.showFloatingCard.set(!this.showFloatingCard());
+  }
 }
